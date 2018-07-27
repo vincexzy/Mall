f71e2630532bf623f2ece935f2a8cb898d2f1702
vincexzy <1091226847@qq.com>
2018-07-27 22:32:50 +0800

    top

--- javascript/main.js
+++ javascript/main.js
@@ -25,3 +25,19 @@ $(".newTab").click(function () {
   $(".job_list").children().css("display","none");
   $(".new_posHotPosition").css("display","block");
 });
+
+$("#backtop").click(function () {
+  $(this).css("background-position-x","-38px");
+  console.log($(window).scrollTop());
+  console.log($(window).height());
+  $("html").animate({
+    scrollTop:0
+  },800);
+});
+$(window).on('scroll', function () {/*当滚动条的垂直位置大于浏览器所能看到的页面的那部分的高度时，回到顶部按钮就显示 */
+  console.log($(window).scrollTop());
+       if ($(window).scrollTop() > 0)
+           $("#backtop").fadeIn();
+       else
+            $("#backtop").fadeOut();
+   });
